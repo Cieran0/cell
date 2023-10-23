32d99105695db8dbc01188946b9a7a02c7194b59
Cieran O'Neill <cieran999@gmail.com>
2023-10-23 17:54:42 +0100

    this doesnt work pplease fix now

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "time.h"
+
+#define CELL_COUNT 100
+
+char cells[CELL_COUNT] = {0};
+char cells_back[CELL_COUNT] = {0};
 
 const char *bit_rep[8] = {
     [0] = "000", [1] = "001", [2] = "010", [3] = "011",
@@ -25,9 +31,66 @@ void gen_map(char rule) {
     
 }
 
+void random_cells() {
+    for (int i = 0; i < CELL_COUNT; i++)
+    {
+        cells[i] = rand()%2;
+    }
+}
+
+void print_cells() {
+    for (int i = 0; i < CELL_COUNT; i++)
+    {
+        printf("%d",cells[i]);
+    }
+    printf("\n");
+}
+
+char concat(char values[]) {
+    char result=0;
+    for (int i = 0; i < 3; i++)
+    {
+        result |= (values[i] << i);
+    }
+    return result;
+}
+
+char* positions_to_values(char positions[]) {
+    char* values = (char*)malloc(sizeof(char)*3);
+    values[0] = cells[positions[0]];
+    values[1] = cells[positions[1]];
+    values[2] = cells[positions[2]];
+    return values;
+}
+
+void do_generation() {
+    char positions[3]={0};
+    for (int i = 0; i < CELL_COUNT; i++)
+    {
+        positions[0]=i+1;
+        positions[1]=i;
+        positions[2]=i-1;
+        for (int j = 0; j < 3; i++)
+        {
+            if(positions[j] >= CELL_COUNT)
+                positions[j]=0;
+            else if(positions[j]<0)
+                positions[j]=CELL_COUNT-1;
+        }
+        cells[i]=map[concat(positions_to_values(positions))];
+    }
+    
+}
+
 int main() {
+    srand(time(NULL));
     gen_map(30);
     char in = 0b001;
     printf("In: %s, Out: %d\n",bit_rep[in & 0x0F],map[in]);
+    random_cells();
+    print_cells();
+    do_generation();
+    print_cells();
+
     return 0;
 }
